a5e6891ce23dac1fa2ca7fdd80fa710e4b25eb8b
nwa24 <nwa24@sfu.ca>
2020-06-01 07:54:31 -0700

    initial commit

---
+++ front-end/src/components/Contact.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>Contact</h1>
+        <p>Here is some information. This is the contact page.</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Contact' // this is the name of the component
+}
+</script>
+
+<style>
+
+</style>
---
+++ front-end/src/components/Nav.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h2>Nav Bar</h2>
+        <nav>
+            <router-link to="/home">Home</router-link>
+            <router-link to="/contact">Contact</router-link>
+            <router-link to="/about">About</router-link>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Navigation'
+}
+</script>
